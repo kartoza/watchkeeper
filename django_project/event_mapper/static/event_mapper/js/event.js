1b6ec953a4e29db6b6c452e4ea3e70605fe62a53
Ismail Sunni <imajimatika@gmail.com>
2015-05-19 11:48:01 +0700

    Select and unselect event.

--- django_project/event_mapper/static/event_mapper/js/event.js
+++ django_project/event_mapper/static/event_mapper/js/event.js
@@ -6,10 +6,12 @@ var markers = [];
 
 var event_layer = new L.LayerGroup();
 var icon_urls = [
-    'static/event_mapper/css/images/blue-blast.png',
-    'static/event_mapper/css/images/red-blast.png'
+    'static/event_mapper/css/images/blast-blue.png',
+    'static/event_mapper/css/images/blast-red.png',
+    'static/event_mapper/css/images/blast-blue-selected.png',
+    'static/event_mapper/css/images/blast-red-selected.png'
 ];
-var pie_chart
+var pie_chart;
 
 var incident_icon = L.icon({
     iconUrl: icon_urls[0],
@@ -18,6 +20,16 @@ var incident_icon = L.icon({
 
 var advisory_icon = L.icon({
     iconUrl: icon_urls[1],
+    iconAnchor: [16, 37]
+});
+
+var selected_incident_icon = L.icon({
+    iconUrl: icon_urls[2],
+    iconAnchor: [16, 37]
+});
+
+var selected_advisory_icon = L.icon({
+    iconUrl: icon_urls[3],
     iconAnchor: [16, 37]
 });
 
@@ -41,13 +53,22 @@ function add_event_marker(event_context){
     }
     if (event_icon) {
         event_marker = L.marker(
-            [lat, lng], {id: event_id, icon: event_icon}).addTo(map);
+            [lat, lng],
+            {
+                id: event_id,
+                icon: event_icon,
+                event_category: event_category,
+                event_place_name: event_place_name,
+                event_selected: false
+            }
+        ).addTo(map);
     }else{
         event_marker = L.marker(
             [lat, lng], {id: event_id}).addTo(map);
     }
 
-    event_marker.bindPopup('loc ' + [lng, lat, event_place_name]);
+    event_marker.on('click', on_click_marker);
+    //event_marker.bindPopup(event_place_name);
 
     // Add to markers
     markers[event_id] = event_marker;
@@ -98,4 +119,48 @@ function create_chart(mdata) {
         }
     ];
     pie_chart = new Chart(container).Pie(data, {animateScale: true});
+}
+
+function on_click_marker(e){
+    var is_selected = this.options.event_selected;
+    for (var i = 0; i < markers.length; i++){
+        if (markers[i]){
+            set_icon(markers[i], false);
+        }
+    }
+    if (is_selected){
+        set_icon(this, false);
+        show_dashboard();
+    } else{
+        set_icon(this, true);
+        show_event_detail(this);
+    }
+}
+
+function set_icon(event, selected){
+    if (event.options.event_category == 1){
+        if (selected){
+            event.setIcon(selected_incident_icon);
+        } else{
+            event.setIcon(incident_icon);
+        }
+    } else if (event.options.event_category == 2){
+        if (selected){
+            event.setIcon(selected_advisory_icon);
+        } else{
+            event.setIcon(advisory_icon);
+        }
+    }
+    event.options.event_selected = selected;
+}
+
+function show_event_detail(event){
+    $('#event_dashboard').hide();
+    $('#event_detail').show();
+    console.log(event.options.event_place_name);
+}
+
+function show_dashboard(){
+    $('#event_dashboard').show();
+    $('#event_detail').hide();
 }
